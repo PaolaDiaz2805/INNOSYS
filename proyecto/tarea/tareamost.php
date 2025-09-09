a624205b073a12569c1607a66076165dea3e515d
PatrickEscudero <escuderoledezmapatrick@pedropoveda.edu.bo>
2025-09-09 14:04:55 -0400

    subir tarea bd
    las tareas ya se guardan en la bd

---
+++ proyecto/tarea/tareamost.php
@@ -0,0 +1,22 @@
+<?php
+$servername="localhost";
+    $username="root";
+    $contraseña="";
+    $dbname="proyecto";
+
+    $conn= new mysqli($servername, $username, $contraseña, $dbname);
+
+    if($conn->connect_error) {
+        echo "Ocurrio un error :( vuelve a intentarlo";
+    }
+    $titulo=$_POST['tt'];
+    $descripcion=$_POST['in'];
+    $tema=$_POST['tem'];
+    $nota=$_POST['pun'];
+    $sql="INSERT INTO tarea (titulo, descripcion, tema, nota, clase_idclase) VALUES('$titulo','$descripcion','$tema','$nota', '3')";
+if ($conn->query($sql) === TRUE) {
+    echo "Se subio la tarea exitosamente";
+    }else{
+        echo "error";
+    }
+?>
